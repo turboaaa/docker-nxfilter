62a5f70d6eccd014cc94c57a1f1ef003f6b33a6c
turboaaa <turboaaa@gmail.com>
2020-04-13 08:38:14 -0400

    Create run.sh
    Fixes turboaaa/docker-nxfilter#9

---
+++ run.sh
@@ -0,0 +1,7 @@
+test='/nxfilter/conf'
+
+##Check if files exist. Create if needed.
+[ ! -d ${test} ] && mv /usr/src/nxfilter_base/* /nxfilter && chown -R nxfilter:nxfilter /nxfilter
+
+##Run nxfilter
+su nxfilter /nxfilter/bin/startup.sh
